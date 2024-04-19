41cd59b7706f0a49f55ff6243b65f14e8a69840f
SuchaJif <68800179+afifihsnm@users.noreply.github.com>
2024-04-19 13:01:29 +0700

    Update course.js

--- controllers/course.js
+++ controllers/course.js
@@ -268,5 +268,5 @@ exports.delete = async (req, res) => {
 };
 
 exports.check = async (req, res) => {
-   res.status(200).send("Its works! version 2");
+   res.status(200).send("Its works! version 3");
 };
